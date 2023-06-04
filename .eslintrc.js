12b18acf71e5ad79996c062e3ad88fc051d30268
ravel01106 <ravello2002@gmail.com>
2023-06-04 16:28:57 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
+    'prettier',
+    'eslint-config-prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+  },
+  settings: {
+		react:{
+			version: 'detect'
+		}
+  }
+}
